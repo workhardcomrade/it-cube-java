4619983b7fd7dc77d59c2ab6f2f2bafbdf225f1d
workhardcomrade <medvedevbogdan@mail.ru>
2022-10-03 06:44:08 +0300

    add practice

---
+++ src/TestClasses/Box.java
@@ -0,0 +1,8 @@
+package TestClasses;
+
+public class Box { // Создаем класс Box, который будет новым типом данных. Box - это шаблон, используемый для создания новых переменных типа Box (объекты класса Box).
+    double width; // Переменная экземпляра класса.
+    double height;
+    double depth;
+
+}
